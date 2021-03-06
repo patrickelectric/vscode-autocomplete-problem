08aae449dceb05d71741b059a63fe613b57a221e
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-06 16:14:26 -0300

    First commit
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use mavlink;
+
+pub fn heartbeat_message() -> mavlink::ardupilotmega::MavMessage {
+    mavlink::ardupilotmega::MavMessage::common({
+        mavlink::common::MavMessage::HEARTBEAT(mavlink::common::HEARTBEAT_DATA {
+            custom_mode: 0,
+            mavtype: mavlink::common::,
+            autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
+            base_mode: mavlink::common::MavModeFlag::empty(),
+            system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
+            mavlink_version: 0x3,
+        })
+    })
+}
+
+fn main() {
+    println!("{:#?}", heartbeat_message());
+}
