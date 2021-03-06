7c9be74f0428c2825a98eeb40a653c28623364eb
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-06 16:26:36 -0300

    src: Add note about problem
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub fn heartbeat_message() -> mavlink::ardupilotmega::MavMessage {
     mavlink::ardupilotmega::MavMessage::common({
         mavlink::common::MavMessage::HEARTBEAT(mavlink::common::HEARTBEAT_DATA {
             custom_mode: 0,
-            mavtype: mavlink::common::,
+            mavtype: mavlink::common::/*does not complete*/,
             autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
             base_mode: mavlink::common::MavModeFlag::empty(),
             system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
